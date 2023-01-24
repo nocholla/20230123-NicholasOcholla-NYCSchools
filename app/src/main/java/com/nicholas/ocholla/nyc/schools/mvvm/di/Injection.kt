5227af79b48a55fded11cc986ed362e3ce4f08b8
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-24 08:58:28 -0800

    Updated Injection Object
    Updated Injection Object

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/di/Injection.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/di/Injection.kt
@@ -1,4 +1,27 @@
 package com.nicholas.ocholla.nyc.schools.mvvm.di
 
+import android.content.Context
+import com.nicholas.ocholla.nyc.schools.mvvm.data.Repository
+import com.nicholas.ocholla.nyc.schools.mvvm.data.remote.ApiService
+import com.nicholas.ocholla.nyc.schools.mvvm.data.remote.RemoteDataSource
+import com.nicholas.ocholla.nyc.schools.mvvm.data.remote.RemoteDataSource.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Inject the dependency to create Repository. Useful for testing.
+ */
 object Injection {
+
+    fun provideTasksRepository(context: Context): Repository {
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        val apiService = retrofit.create(ApiService::class.java)
+        return Repository.getInstance(RemoteDataSource.getInstance(apiService))
+    }
+    
 }
