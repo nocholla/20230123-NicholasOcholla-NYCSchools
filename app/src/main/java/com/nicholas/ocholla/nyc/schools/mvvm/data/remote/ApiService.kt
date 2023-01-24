181fcb2514528903f2dc84c6e05281511e1f2019
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-23 20:04:58 -0800

    Update ApiService.kt
    Added ApiService

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/data/remote/ApiService.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/data/remote/ApiService.kt
@@ -1,4 +1,20 @@
 package com.nicholas.ocholla.nyc.schools.mvvm.data.remote
 
+import com.nicholas.ocholla.nyc.schools.mvvm.data.model.Schools
+import com.nicholas.ocholla.nyc.schools.mvvm.data.model.Scores
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
+/**
+ * Api call for retrofit.
+ */
 interface ApiService {
+
+    @GET("resource/s3k6-pzi2.json")
+    fun getSchools(@QueryMap queryMap: Map<String, String>): Call<Schools>
+
+    @GET("resource/f9bf-2cp4.json")
+    fun getScores(@QueryMap queryMap: Map<String, String>): Call<Scores>
+
 }
