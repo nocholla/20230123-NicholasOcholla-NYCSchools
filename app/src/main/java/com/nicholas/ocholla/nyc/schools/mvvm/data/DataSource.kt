c3a0d0a5570c01abda2527bafbe6db6d84054962
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-23 20:08:55 -0800

    Updated DataSource
    Updated DataSource

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/data/DataSource.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/data/DataSource.kt
@@ -1,4 +1,17 @@
 package com.nicholas.ocholla.nyc.schools.mvvm.data
 
+import com.nicholas.ocholla.nyc.schools.mvvm.data.model.Schools
+
+/**
+ * Main entry point for accessing data.
+ */
 interface DataSource {
+
+    interface GetSchoolsCallback {
+        fun onOperationComplete(schools: Schools)
+        fun onOperationFailed(t: Throwable? = Throwable())
+    }
+
+    fun getSchools(page: Int = 1, callback: GetSchoolsCallback)
+
 }
--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/data/remote/RemoteDataSource.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/data/remote/RemoteDataSource.kt
@@ -1,17 +1,4 @@
 package com.nicholas.ocholla.nyc.schools.mvvm.data.remote
 
-import com.nicholas.ocholla.nyc.schools.mvvm.data.model.Schools
-
-/**
- * Main entry point for accessing data.
- */
-interface DataSource {
-
-    interface GetSchoolsCallback {
-        fun onOperationComplete(schools: Schools)
-        fun onOperationFailed(t: Throwable? = Throwable())
-    }
-
-    fun getSchools(page: Int = 1, callback: GetSchoolsCallback)
-
+class RemoteDataSource {
 }
