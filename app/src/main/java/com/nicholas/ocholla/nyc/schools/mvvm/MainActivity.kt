eb57c08b1d199d7c40638fef63f00ff6f407f594
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-25 11:30:58 -0800

    Updated MainActivity

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/MainActivity.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/MainActivity.kt
@@ -2,10 +2,59 @@ package com.nicholas.ocholla.nyc.schools.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nicholas.ocholla.nyc.schools.mvvm.view.SchoolListAdapter
+import com.nicholas.ocholla.nyc.schools.mvvm.viewmodel.ListViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: ListViewModel
+    private val schoolsAdapter = SchoolListAdapter(this, arrayListOf())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProviders.of(this)[ListViewModel::class.java]
+        viewModel.refresh()
+
+        schoolsList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = schoolsAdapter
+        }
+
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            viewModel.refresh()
+        }
+
+        observeViewModel()
     }
+
+    private fun observeViewModel() {
+        viewModel.schools.observe(this, Observer {schools ->
+            schools?.let {
+                schoolsList.visibility = View.VISIBLE
+                schoolsAdapter.updateSchools(it) }
+        })
+
+        viewModel.schoolLoadError.observe(this, Observer { isError ->
+            isError?.let { list_error.visibility = if(it) View.VISIBLE else View.GONE }
+        })
+
+        viewModel.loading.observe(this, Observer { isLoading ->
+            isLoading?.let {
+                loading_view.visibility = if(it) View.VISIBLE else View.GONE
+                if(it) {
+                    list_error.visibility = View.GONE
+                    schoolsList.visibility = View.GONE
+                }
+            }
+        })
+    }
+
 }
