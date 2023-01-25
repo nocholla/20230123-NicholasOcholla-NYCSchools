8dfe6617e405cf46a007eba25dcb52b2c7ff4060
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-24 17:59:26 -0800

    Added Scores Service
    Added Scores Service

---
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/model/ScoresService.kt
@@ -0,0 +1,20 @@
+package com.nicholas.ocholla.nyc.schools.mvvm.model
+
+//import com.nicholas.ocholla.nyc.schools.mvvm.di.DaggerApiComponent
+import io.reactivex.Single
+import javax.inject.Inject
+
+class ScoresService {
+
+    @Inject
+    lateinit var api: ScoresApi
+
+    init {
+        //DaggerApiComponent.create().inject(this)
+    }
+
+    fun getScores(): Single<List<Score>> {
+        return api.getScores()
+    }
+
+}
