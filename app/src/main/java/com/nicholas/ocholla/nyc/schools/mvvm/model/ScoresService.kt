eaf546105a2753c49ff8a06346117b2e066ff42d
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-25 10:42:52 -0800

    Added ListViewModel
    Added ListViewModel

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/model/ScoresService.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/model/ScoresService.kt
@@ -1,6 +1,6 @@
 package com.nicholas.ocholla.nyc.schools.mvvm.model
 
-//import com.nicholas.ocholla.nyc.schools.mvvm.di.DaggerApiComponent
+import com.nicholas.ocholla.nyc.schools.mvvm.di.DaggerApiComponent
 import io.reactivex.Single
 import javax.inject.Inject
 
@@ -10,7 +10,7 @@ class ScoresService {
     lateinit var api: ScoresApi
 
     init {
-        //DaggerApiComponent.create().inject(this)
+        DaggerApiComponent.create().inject(this)
     }
 
     fun getScores(): Single<List<Score>> {
--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/viewmodel/ListViewModel.kt
@@ -1,7 +1,90 @@
 package com.nicholas.ocholla.nyc.schools.mvvm.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.nicholas.ocholla.nyc.schools.mvvm.di.DaggerApiComponent
+import com.nicholas.ocholla.nyc.schools.mvvm.model.School
+import com.nicholas.ocholla.nyc.schools.mvvm.model.SchoolsService
+import com.nicholas.ocholla.nyc.schools.mvvm.model.Score
+import com.nicholas.ocholla.nyc.schools.mvvm.model.ScoresService
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
 class ListViewModel: ViewModel() {
+
+    @Inject
+    lateinit var schoolsService: SchoolsService
+
+    @Inject
+    lateinit var scoresService: ScoresService
+
+    init {
+        DaggerApiComponent.create().inject(this)
+    }
+
+    private val disposable = CompositeDisposable()
+
+    val schools = MutableLiveData<List<School>>()
+    val scores = MutableLiveData<List<Score>>()
+    val schoolLoadError = MutableLiveData<Boolean>()
+    val scoreLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        fetchSchools()
+        fetchScores()
+    }
+
+    private fun fetchSchools() {
+        loading.value = true
+        disposable.add(
+            schoolsService.getSchools()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object: DisposableSingleObserver<List<School>>() {
+                    override fun onSuccess(value: List<School>?) {
+                        schools.value = value
+                        schoolLoadError.value = false
+                        loading.value = false
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        schoolLoadError.value = true
+                        loading.value = false
+                    }
+
+                })
+        )
+    }
+
+    private fun fetchScores() {
+        loading.value = true
+        disposable.add(
+            scoresService.getScores()
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(object: DisposableSingleObserver<List<Score>>() {
+                    override fun onSuccess(value: List<Score>?) {
+                        scores.value = value
+                        scoreLoadError.value = false
+                        loading.value = false
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        scoreLoadError.value = true
+                        loading.value = false
+                    }
+
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 
 }
