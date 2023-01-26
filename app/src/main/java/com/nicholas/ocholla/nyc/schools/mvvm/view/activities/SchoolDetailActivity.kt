06ab8bbde0fc92f88fbdfefd554e5cbf6821eb67
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2023-01-25 20:25:38 -0800

    Updated School Detail Activity
    Updated School Detail Activity

--- app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/view/activities/SchoolDetailActivity.kt
+++ app/src/main/java/com/nicholas/ocholla/nyc/schools/mvvm/view/activities/SchoolDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.nicholas.ocholla.nyc.schools.mvvm.view.activities
 
 import android.annotation.SuppressLint
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ActivityInfo
@@ -8,8 +9,10 @@ import android.content.res.Configuration
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Parcelable
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
@@ -71,17 +74,129 @@ class SchoolDetailActivity : AppCompatActivity() {
         }
 
         shareCV.addDebouncedClickListener {
+            val targetShareIntents = ArrayList<Intent>()
+            val shareIntent = Intent()
+            shareIntent.action = Intent.ACTION_SEND
+            shareIntent.type = "text/plain"
 
+            val pm = this.packageManager
+            val resInfos = pm?.queryIntentActivities(shareIntent, 0)
+
+            if (resInfos != null) {
+                if (resInfos.isNotEmpty()) {
+                    for (resInfo in resInfos) {
+
+                        val packageName = resInfo.activityInfo.packageName
+
+                        if (packageName.contains("com.google.android.gm") || packageName.contains("com.yahoo.mobile")) {
+
+                            val intent = Intent()
+                            intent.component = ComponentName(packageName, resInfo.activityInfo.name)
+                            intent.putExtra("AppName", resInfo.loadLabel(pm).toString())
+                            intent.action = Intent.ACTION_SEND
+                            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+                            intent.type = "text/plain"
+                            intent.putExtra(Intent.EXTRA_SUBJECT, "Parent Feedback!")
+                            intent.putExtra(Intent.EXTRA_TEXT, "Hello, NYC City Schools")
+                            intent.setPackage(packageName)
+                            targetShareIntents.add(intent)
+
+                        }
+                    }
+
+                    if (targetShareIntents.isNotEmpty()) {
+                        targetShareIntents.sortWith(Comparator { o1, o2 ->
+                            o2.getStringExtra("AppName")?.let {
+                                o1.getStringExtra("AppName")
+                                    ?.compareTo(it)
+                            }!!
+                        })
+                        val chooserIntent = Intent.createChooser(targetShareIntents.removeAt(0), "Send Feedback Via")
+                        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetShareIntents.toTypedArray<Parcelable>())
+                        this.startActivity(chooserIntent)
+                    } else {
+                        Toast.makeText(this, getString(R.string.toast_no_app_to_share), Toast.LENGTH_LONG).show()
+                    }
+
+                }
+            }
         }
 
         socialCV.addDebouncedClickListener {
-
+            shareSocial()
         }
 
         websiteCV.addDebouncedClickListener {
             // Open School Website
             val urlIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://$website"))
             startActivity(urlIntent)
+        }
+
+    }
+
+    private fun shareSocial() {
+
+        val targetShareIntents = ArrayList<Intent>()
+        val shareIntent = Intent()
+        shareIntent.action = Intent.ACTION_SEND
+        shareIntent.type = "text/plain"
+
+        val pm = this.packageManager
+        val resInfos = pm?.queryIntentActivities(shareIntent, 0)
+        if (!resInfos?.isEmpty()!!) {
+            for (resInfo in resInfos) {
+
+                val packageName = resInfo.activityInfo.packageName
+
+                if (packageName.contains("com.twitter.android")
+                    || packageName.contains("com.facebook.katana")
+                    || packageName.contains("com.whatsapp")
+                    || packageName.contains("com.google.android.apps.plus")
+                    || packageName.contains("com.google.android.talk")
+                    || packageName.contains("com.slack")
+                    || packageName.contains("com.google.android.gm")
+                    || packageName.contains("com.facebook.orca")
+                    || packageName.contains("com.yahoo.mobile")
+                    || packageName.contains("com.skype.raider")
+                    || packageName.contains("com.android.mms")
+                    || packageName.contains("com.linkedin.android")
+                    || packageName.contains("com.google.android.apps.messaging")) {
+
+                    val intent = Intent()
+                    intent.component = ComponentName(packageName, resInfo.activityInfo.name)
+                    intent.putExtra("AppName", resInfo.loadLabel(pm).toString())
+                    intent.action = Intent.ACTION_SEND
+                    //intent.setType("image/*");
+                    intent.type = "text/plain"
+                    intent.putExtra(Intent.EXTRA_SUBJECT, "Check out the NYC Schools")
+                    intent.putExtra(
+                        Intent.EXTRA_TEXT, "NYC Schools"
+                                + " \n "
+                                + " \n "
+                                + "Get more info about NYC Schools"
+                                + " \n "
+                                + " \n "
+                                + "Thank you!")
+
+                    intent.setPackage(packageName)
+                    targetShareIntents.add(intent)
+
+                }
+            }
+            if (!targetShareIntents.isEmpty()) {
+                targetShareIntents.sortWith(Comparator { o1, o2 ->
+                    o2.getStringExtra("AppName")?.let {
+                        o1.getStringExtra("AppName")
+                            ?.compareTo(it)
+                    }!!
+                })
+                val chooserIntent = Intent.createChooser(targetShareIntents.removeAt(0), "Share Via")
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, targetShareIntents.toTypedArray<Parcelable>())
+                this.startActivity(chooserIntent)
+            } else {
+                Toast.makeText(this, getString(R.string.toast_no_app_to_share), Toast.LENGTH_LONG).show()
+            }
+
         }
 
     }
